e9af1d18aa58ab85a2009ebbd48a087257081c5b
Ahmad Mustafa <ahmad149b@gmail.com>
2026-03-18 00:35:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ import streamlit as st
 import pickle
 import string
 from nltk.corpus import stopwords
-import nltk
 from nltk.stem.porter import PorterStemmer
+import nltk
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+    nltk.download('punkt_tab')
+    nltk.download('stopwords')
 
 ps = PorterStemmer()
 
